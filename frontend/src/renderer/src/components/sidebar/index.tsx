c8905a93beaa22b47eff399dd78c9b07125d7b5e
Noel George C J <noelgeorge2004@gmail.com>
2025-03-15 17:36:05 +0530

    Sidebar structure

--- frontend/src/renderer/src/components/sidebar/index.tsx
+++ frontend/src/renderer/src/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Button } from "@renderer/components/ui/button"
+import avatar from "../../assets/self.jpg"
 import {
     Card,
     CardContent,
@@ -13,7 +14,13 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupAction,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
   } from "@renderer/components/ui/sidebar"
   import {
     Command,
@@ -30,7 +37,25 @@ import {
   } from "@renderer/components/ui/popover"
 import { Avatar, AvatarFallback, AvatarImage } from '@renderer/components/ui/avatar'
 import { cn } from '@renderer/lib/utils'
-import { Check, ChevronsUpDown } from 'lucide-react'
+import { Check, ChevronsUpDown, CircleDot, Dot, Plus } from 'lucide-react'
+
+const branches = [
+  {
+    name: "Main",
+    url: "#",
+    icon: () => <CircleDot />,
+  },
+  {
+    name: "Feature",
+    url: "#",
+    icon: () => <Dot />,
+  },
+  {
+    name: "Bugfix",
+    url: "#",
+    icon: () => <Dot />,
+  }
+]
 
 const repositories = [
   {
@@ -61,6 +86,9 @@ type CardProps = React.ComponentProps<typeof Card>
 const SideBar = ({ className, ...props }: CardProps) => {
   const [repoMenuOpen, setRepoMenuOpen] = React.useState(false)
   const [currentRepo, setCurrentRepo] = React.useState("")
+
+  const [profileMenuOpen, setProfileMenuOpen] = React.useState(false)
+  const [currentProfile, setCurrentProfile] = React.useState("")
   return (
     <Sidebar>
       <SidebarHeader>
@@ -113,10 +141,77 @@ const SideBar = ({ className, ...props }: CardProps) => {
         </Popover>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Branches</SidebarGroupLabel>
+          <SidebarGroupAction title="Add Branch">
+            <Plus /> <span className="sr-only">Add Branch</span>
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {branches.map((branch) => (
+                <SidebarMenuItem key={branch.name}>
+                  <SidebarMenuButton asChild>
+                    <a href={branch.url}>
+                      <branch.icon />
+                      <span>{branch.name}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>     
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <Popover open={profileMenuOpen} onOpenChange={setProfileMenuOpen}>
+          <PopoverTrigger>
+            <Button
+              variant="outline"
+              role="combobox"
+              aria-expanded={profileMenuOpen}
+              className="w-full py-6 cursor-pointer justify-between"
+            >
+              <Avatar>
+                <AvatarImage src={avatar} />
+                <AvatarFallback>VC</AvatarFallback>
+              </Avatar>
+              {currentProfile
+                ? repositories.find((repository) => repository.value === currentProfile)?.label
+                : "Noel George"}
+              <ChevronsUpDown className="opacity-50" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-[250px] p-0">
+            <Command>
+              <CommandInput placeholder="Search repository..." className="h-9" />
+              <CommandList>
+                <CommandEmpty>No framework found.</CommandEmpty>
+                <CommandGroup>
+                  {repositories.map((repository) => (
+                    <CommandItem
+                      key={repository.value}
+                      value={repository.value}
+                      onSelect={(currentValue) => {
+                        setCurrentRepo(currentValue === currentRepo ? "" : currentValue)
+                        setRepoMenuOpen(false)
+                      }}
+                    >
+                      {repository.label}
+                      <Check
+                        className={cn(
+                          "ml-auto",
+                          currentRepo === repository.value ? "opacity-100" : "opacity-0"
+                        )}
+                      />
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+      </SidebarFooter>
     </Sidebar>
   )
 }
